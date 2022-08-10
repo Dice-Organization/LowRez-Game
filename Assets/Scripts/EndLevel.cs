2d946f4accd61fcb8d338a201c6df6964b4595a6
Fasith-23 <89504643+Fasith-23@users.noreply.github.com>
2022-08-10 23:30:14 +0530

    portal+newlevel

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EndLevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Blended")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
