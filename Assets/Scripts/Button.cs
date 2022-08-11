75168389da7bec06f03d5a32aaa1b06a983b4566
D1ceK1ng <104449740+D1ceK1ng@users.noreply.github.com>
2022-08-11 17:56:51 +0300

    Added Camera tip

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,19 +1,27 @@
 
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using System.Collections;
+using System;
 
 public class Button : MonoBehaviour
 {
     public List<GameObject> LinkedDoors;
     public  GameObject LinkedPlayerForm;
+    public CinemachineTargetGroup TargetGroup;
 
     private bool _isPressed;
     private bool _canInteract;
     private Interact _playerInteract;
     private Animator _animator;
 
+    private CinemachineBrain _brain;
+    private CinemachineVirtualCamera _activeCamera;
+
     private void Awake() 
     {
+        _brain = Camera.main.GetComponent<CinemachineBrain>();
         _playerInteract = LinkedPlayerForm.GetComponent<Interact>();
         _animator = GetComponent<Animator>();
         
@@ -21,19 +29,24 @@ public class Button : MonoBehaviour
         _playerInteract.PlayerInteract += OpenDoor;
     }
 
+  
+
     public void OpenDoor()
     {
-        
         if(_isPressed || !_canInteract)
             return;
 
          foreach (GameObject linkedDoor in LinkedDoors)
-        {
+        {            
+            _activeCamera.Follow = linkedDoor.transform;
             linkedDoor.SetActive(false);
         }
 
+
         _isPressed = true;
         _canInteract = false;
+
+        
         _animator.SetTrigger("IsPressed");
     }
 
@@ -50,10 +63,19 @@ public class Button : MonoBehaviour
        if(other.tag == LinkedPlayerForm.tag)
         {
             _canInteract = false;
+            ReturnCamera();
         } 
     }
     private void Update() 
     {
         _animator.SetBool("IsInRange",_canInteract);
+        _activeCamera = (CinemachineVirtualCamera)_brain.ActiveVirtualCamera;
+
+        
+    }
+
+    private void ReturnCamera()
+    {
+        _activeCamera.Follow = LinkedPlayerForm.transform;
     }
 }
